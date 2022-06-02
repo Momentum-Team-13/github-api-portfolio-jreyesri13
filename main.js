00e7c2b6baebb63c3847901210419dbfc91b7533
Jose Reyes <jreyesri@cbu.edu>
2022-06-02 11:04:17 -0400

    Made HTML, CSS, and JS basics

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log('js is connected!')
+
+// let gitHubUrl = 
+
+fetch(gitHubUrl, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' }
+})
+    .then(function (response) {
+        // the response is the promised data
+        return response.json()
+        // put the response in JSON format
+    })
+    .then(function (data) {
+        // data refers to what the above promise returned (response.json())
+        console.log("Response from GitHub API: ", data.name)
+        // console log the data
+        buildProfile(data)
+
+    })
+
+function buildProfile(profileData) {
+    console.log(profileData)
+    // create elements and add them to the page
+    // profileData is the data from the promise
+}
